75eb1dd943c62b517e8a8720b8d3aa055f6f0871
Marwan Tammam <marwan.01010100@gmail.com>
2018-03-21 18:41:49 +0200

    Add beta implementation of machine

--- machine.cpp
+++ machine.cpp
@@ -4,72 +4,117 @@
 
 #include "machine.h"
 
-void state::transition::get_end_state() const {
-    return;
+state::transition::transition(const transition &copy) {
+    this->on_input = copy.get_transition_char();
+    this->to = new state(*copy.get_end_state());
 }
 
-void state::transition::get_transition_char() const {
-    return;
+state::transition::transition(state *to, char on_input) {
+    this->to = to;
+    this->on_input = on_input;
 }
 
+state *state::transition::get_end_state() const {
+    return this->to;
+}
+
+char state::transition::get_transition_char() const {
+    return this->on_input;
+}
+
+state::state() {
+}
+
+state::state(const state &copy) {
+    this->state_identifier = copy.get_state_identifier();
+    this->token_class = copy.get_token_class();
+    this->_is_accepting = copy.is_accepting();
+
+    for (std::pair<const char, std::vector<transition> > &entry : copy.get_transitions()) {
+        char c = entry.first;
+        this->transitions[c] = std::vector<transition>();
+        for (transition &t : entry.second){
+            this->transitions[c].push_back(transition(t));
+        }
+    }
+}
 
 int state::get_state_identifier() const {
-    return 0;
+    return this->state_identifier;
 }
 
 std::string state::get_token_class() const {
-    return NULL;
+    return this->token_class;
 }
 
 bool state::is_accepting() const {
-    return false;
+    return this->_is_accepting;
 }
 
-void state::set_accepting(bool) {
-    return;
+void state::set_accepting(bool is_accepting) {
+    this->_is_accepting = is_accepting;
 }
 
-std::vector<state::transition> state::get_transitions_for(char in) const {
-    return std::vector<state::transition>();
+std::vector<state::transition> state::get_transitions_for(char in) {
+    return this->transitions[in];
+}
+
+std::map<char, std::vector<state::transition> > state::get_transitions() const {
+    return this->transitions;
 }
 
 void state::add_new_transition(state to, char on_input) {
-    return;
+    if (this->transitions.count(on_input) == 0) {
+        this->transitions[on_input] = std::vector<transition>();
+    }
+    this->transitions[on_input].push_back(transition(&to, on_input));
 }
 
 
 machine::machine(std::string _machine_identifier) {
-    return;
+    this->machine_identifier = _machine_identifier;
+    this->states = std::vector<state>();
 }
 
 machine::machine(const machine &copy) {
-    return;
+    this->machine_identifier = copy.get_machine_identifier();
+    this->starting = new state(copy.get_starting_state());
+
+    for (state &s : copy.get_states()){
+        this->states.push_back(state(s));
+    }
 }
 
-void machine::add_new_state(state) {
-    return;
+void machine::add_new_state(state s) {
+    this->states.push_back(s);
 }
 
 void machine::add_new_transition(state from, state to, char on_input) {
-    return;
+    from.add_new_transition(to, on_input);
 }
 
 void machine::set_starting_state(state *_starting) {
-    return;
+    this->starting = _starting;
 }
 
 state machine::get_starting_state() const {
-    return state();
+    return *(this->starting);
 }
 
 std::string machine::get_machine_identifier() const {
-    return NULL;
+    return this->machine_identifier;
 }
 
 std::vector<state> machine::get_states() const {
-    return std::vector<state>();
+    return this->states;
 }
 
 std::vector<state> machine::get_accepting_states() const {
-    return std::vector<state>();
+    std::vector<state> accepting_states;
+    for (const state &s : this->states) {
+        if (s.is_accepting()) {
+            accepting_states.push_back(s);
+        }
+    }
+    return accepting_states;
 }
--- machine.h
+++ machine.h
@@ -9,53 +9,79 @@
 #include <string>
 #include <vector>
 #include <map>
+
 #define EPS 0xDE
 
 class state {
 
-  class transition {
-   private:
-    state *to;
-    char on_input;
-   public:
-    void get_end_state () const;
-    void get_transition_char () const;
-  };
- private:
-  int state_identifier;
-  std::string token_class;
-  bool _is_accepting;
-  std::map<char, std::vector<transition> > transitions;
- public:
-  int get_state_identifier () const;
-  std::string get_token_class () const;
-  bool is_accepting () const;
-  void set_accepting (bool);
-  std::vector<transition> get_transitions_for (char in) const;
-  /*
-   * Adds a new transition from this state to TO state.
-   */
-  void add_new_transition (state to, char on_input = EPS);  // 0xDE = Epsilon
+    class transition {
+    private:
+        state *to;
+        char on_input;
+    public:
+        transition(const transition &);
+
+        transition(state *to, char on_input);
+
+        state *get_end_state() const;
+
+        char get_transition_char() const;
+    };
+
+private:
+    int state_identifier;
+    std::string token_class;
+    bool _is_accepting;
+    std::map<char, std::vector<transition> > transitions;
+public:
+    state(const state &copy);
+
+    state();
+
+    int get_state_identifier() const;
+
+    std::string get_token_class() const;
+
+    bool is_accepting() const;
+
+    void set_accepting(bool);
+
+    std::vector<transition> get_transitions_for(char in);
+
+    std::map<char, std::vector<state::transition> > get_transitions() const;
+
+    /*
+     * Adds a new transition from this state to TO state.
+     */
+    void add_new_transition(state to, char on_input = EPS);  // 0xDE = Epsilon
 };
 
 class machine {
 
- private:
-  std::string machine_identifier;
-  state *starting;
-  std::vector<state> states;
+private:
+    std::string machine_identifier;
+    state *starting;
+    std::vector<state> states;
 
- public:
-  machine (std::string _machine_identifier);
-  machine (const machine& copy);
+public:
+    machine(std::string _machine_identifier);
+
+    machine(const machine &copy);
+
 //  void add_new_state (int state_identifier, std::string token_class, bool is_accepting = false);
-  void add_new_state (state);
-  void add_new_transition (state from, state to, char on_input = EPS);
-  void set_starting_state (state *_starting);
-  state get_starting_state () const;
-  std::string get_machine_identifier () const;
-  std::vector<state> get_states () const;
-  std::vector<state> get_accepting_states () const;
+    void add_new_state(state);
+
+    void add_new_transition(state from, state to, char on_input = EPS);
+
+    void set_starting_state(state *_starting);
+
+    state get_starting_state() const;
+
+    std::string get_machine_identifier() const;
+
+    std::vector<state> get_states() const;
+
+    std::vector<state> get_accepting_states() const;
 };
 
 #endif //KOMPILER_MACHINE_H
