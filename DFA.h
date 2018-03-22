3e14e0595f0219c1bd0a28419ccfbd415ffc8e3c
Anas Harby <anas.harby@gmail.com>
2018-03-22 21:14:11 +0200

    Refactor dfa.h

--- DFA.h
+++ DFA.h
@@ -1,12 +1,13 @@
 #ifndef KOMPILER_DFA_H
 #define KOMPILER_DFA_H
 #include "machine.h"
+typedef int sid_t;
 
 namespace dfa {
-std::set<state> epsilon_closure(state state);
-std::set<state> epsilon_closure(std::set<state> states);
-std::set<state> move(state states, char in);
-std::set<state> move(std::set<state> states, char in);
+std::set<sid_t> epsilon_closure(machine m, sid_t s);
+std::set<sid_t> epsilon_closure(machine m,, std::set<sid_t> state_set);
+std::set<sid_t> move(machine m, sid_t s, char in);
+std::set<sid_t> move(machine m, std::set<sid_t> state_set, char in);
 machine* to_dfa(machine &nfa);
 machine* minimize_dfa(machine &dfa);
 }
