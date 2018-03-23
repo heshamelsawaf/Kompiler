98bf065b8cc836209afa32b10a83a1fd87132c8b
Mohammed Abdelbarry <m7md337@gmail.com>
2018-03-24 00:24:45 +0200

    Added parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,16 @@
+#include "parser.h"
+
+parser::parser(std::ifstream *_ifs, lexer &_lex) : lex{_lex} {
+    ifs = _ifs;
+}
+
+
+std::vector<lexer::token> parser::parse() {
+    std::vector<lexer::token> v;
+    lexer::token tok ("", "");
+    do {
+        tok = lex.next_token(*ifs);
+        v.push_back(tok);
+    } while (tok.get_class() != "EOFSPECIALTOKENVIPONLY");
+    return v;
+}
