84d74050a2a513654cacca894543761cf24920e0
Anas Harby <anas.harby@gmail.com>
2018-03-23 21:37:16 +0200

    Implement lexical tokenization

--- lexer.cpp
+++ lexer.cpp
@@ -4,10 +4,14 @@
 #include "rexparser.h"
 #include "dfa.h"
 
-lexer::transition_table lex_analyze(std::ifstream &ifs, bool verbose) {
-    auto s = [&ifs]{
+lexer::lexer(const std::ifstream &_ifs) {
+    rules_ifs = _ifs;
+}
+
+void lexer::lex_analyze() {
+    auto s = [&rules_ifs] {
       std::ostringstream ss;
-      ss << ifs.rdbuf();
+      ss << rulse_ifs.rdbuf();
       return ss.str();
     }();
 
@@ -20,18 +24,43 @@ lexer::transition_table lex_analyze(std::ifstream &ifs, bool verbose) {
     machne  dfa = dfa::to_dfa(nfa);
     machine min_dfa = dfa::minimize_dfa(dfa);
 
-    return transition_table(min_dfa);
+    ttab = transition_table(min_dfa);
 }
 
-lexer::token next_token(std::ifstream &ifs, transition_table &ttab) {
+
+lexer::token next_token(std::ifstream &ifs) {
+    std::ostringstream accum_ss;
+    std::ostringstream token_ss;
+    std::string token_class = "error";
+
     if (ifs.eof() || !ifs.is_open())
         return NULL;
 
-    char c;
-    if (!ifs >> s)
+    char c = ifs.peek();
+    if (c == EOF)
         return NULL;
 
+    while (isspace((char) ifs.peek()))
+        ifs >> c;
 
+    while (ifs.peek()) {
+        accum_ss << ifs.get(c);
+
+        sid_t st = ttab.move(c);
+        if (st == -1)    // reached a deadend state
+            break;
+
+        else if (ttab.is_accepting()) {
+            // append accum_ss to token_ss, clear accum_ss afterwards
+            token_ss << accum_ss;
+            accum_ss.str(""); accum_ss.clear();
+
+            token_class = ttab.get_token_class();
+        }
+    }
+
+    return token(token_class.empty() ? accum_ss.str() : token_ss.str(),
+            token_class);
 }
 
 std::vector<lexer::token> tokenize(std::ifstream &ifs, transition_table &ttab) {
@@ -48,4 +77,25 @@ std::vector<lexer::token> tokenize(std::ifstream &ifs, transition_table &ttab) {
 
     }
 
+}
+
+
+lexer::token next_token(const std::string &str,
+                        transition_table ttab, int &ptr) {
+    if (ptr == NULL || ptr >= str.size())
+        return NULL;
+
+    std::ostringstream ss;
+    ttab.reset();
+
+    while (isspace(str[ptr] && ptr < str.size())
+        ptr++;
+
+    while (!isspace(str[ptr] && ptr < str.size()) {
+        ss << str[ptr];
+        ttab.move(str[ptr]);
+        ptr++;
+    }
+
+    return lexer::token(ss.str(), get_token_class());
 }
--- lexer.h
+++ lexer.h
@@ -5,74 +5,68 @@
 #include "machine.h"
 
 class lexer {
+public:
+        class token {
+        private:
+            std::string t_str;
+            std::string t_class;
+        public:
+            token(std::string _str, std::string _class) {
+                t_str = _str;
+                t_class = _class;
+            }
 
-    class token {
-    private:
-        std::string t_str;
-        std::string t_class;
-    public:
-        token(std::string _str, std::string _class) {
-            t_str = _str;
-            t_class = _class;
-        }
+            std::string get_str() {
+                return t_str;
+            }
 
-        std::string get_str() {
-            return t_str;
-        }
+            std::string get_class() {
+                return t_class;
+            }
+        };
 
-        std::string get_class() {
-            return t_class;
-        }
-    };
+        class transition_table {
+        private:
+            machine m;
+            sid_t cur_state;
+        public:
+            transition_table(machine _m) {
+                m = _m;
+                cur_state = m.get_starting_state();
+            }
 
-    class transition_table {
-    private:
-        machine m;
-        sid_t cur_state;
-    public:
-        transition_table(machine _m) {
-            m = _m;
-            cur_state = m.get_starting_state();
-        }
+            sid_t get_current_state() {
+                return cur_state;
+            }
 
-        sid_t get_current_state() {
-            return cur_state;
-        }
+            sid_t move(char in) {
+                std::vector<sid_t> transition = m.get_transitions(cur_state, in);
+                if (transition.empty())
+                    return -1;
+                cur_state = m.get_transitions(cur_state, in)[0];
+                return cur_state;
+            }
 
-        sid_t move(char in) {
-            std::vector<sid_t> transition = m.get_transitions(cur_state, in);
-            if (transition.empty())
-                return -1;
-            cur_state = m.get_transitions(cur_state, in)[0];
-            return cur_state;
-        }
+            bool is_accepting() {
+                return m.is_accepting(cur_state);
+            }
 
-        bool has_transition(sid_t state, c) {
-            std::vector<sid_t> transitions = m.get_transitions(state, in);
-            if (transition.empty())
-                return false;
-            return true;
-        }
+            std::string get_token_class() {
+                return m.get_token_class(cur_state);
+            }
 
-        bool is_accepting() {
-            return m.is_accepting(cur_state);
-        }
-
-        std::string get_token_class() {
-            return m.get_token_class(cur_state);
-        }
-
-        sid_t reset() {
-            cur_state = m.get_starting_state();
-            return cur_state;
-        }
-    };
+            sid_t reset() {
+                cur_state = m.get_starting_state();
+                return cur_state;
+            }
+        };
 private:
-    std::ifstream
+    std::ifstream rules_ifs;
+    transition_table ttab;
 
 public:
-    lexer(const std::ifstream &ifs);
-    lex_analyze();
+    lexer(const std::ifstream &_ifs);
+    void lex_analyze();
     token next_token(const std::ifstream &ifs);
 };
 
