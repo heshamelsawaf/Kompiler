68d370c4b91dd5108c2d99d6076627de2a0c70ac
Anas Harby <anas.harby@gmail.com>
2018-04-20 22:04:21 +0200

    Implement First set computing - Implement first set computation, it is noted that the signature may be changed in future - Add function build() which is used to build CFG after adding productions and symbols to CFG

--- cfg.cpp
+++ cfg.cpp
@@ -1,8 +1,59 @@
-//
-// Created by hsawaf on 4/20/18.
-//
+#include "cfg.h"
 
+bool build_first_util(cfg &grmr) {
+    bool done = false;
 
-int main() {
+    for (std::string sym_key : grmr.get_symbols()) {
+        cfg::symbol *s = grmr.get_symbol(sym_key);
 
+        if (s->is_terminal())
+            continue;
+        
+        for (cfg::symbol::production prod : s->get_productions()) {
+            /* For each production S->T where T is non-terminal,
+             * Add FIRST(T) to FIRST(S) */
+            cfg::symbol *t = *prod.get_symbols().begin();
+            if (!t->is_terminal())
+                for (std::string a: t->get_first())
+                    done ^= prod.add_first(a);
+        }
+    }
+    
+    return done;
+}
+
+void build_first(cfg &grmr) {
+    /* initialize FIRST sets for symbols */
+    for (std::string sym_key : grmr.get_symbols()) {
+        cfg::symbol *s = grmr.get_symbol(sym_key);
+        
+        /* If symbol S is terminal, then FIRST(S) = { S } */
+        if (s->is_terminal())
+            continue;
+
+        /* If symbol S is non-terminal, add all terminals in the
+           beginning of its productions (including EPS) to its first set */
+        for (cfg::symbol::production prod : s->get_productions()) {
+            cfg::symbol *t = *prod.get_symbols().begin();
+            if (t->is_terminal())
+                prod.add_first(t->get_key());
+        }
+    }
+
+    /* For non-terminals S, A where A->S, FIRST(S) is
+       a subset of FIRST(A), add all of FIRST(A) to FIRST(S),
+       repeat until no changes can be made */
+    while (build_first_util(grmr));
+}
+
+void build_follow(cfg &grmr) {
+
+}
+
+void cfg::build() {
+    /* TODO: either make `build_first()`,
+     * `build_follow()` non-static member functions
+     * or separate them outside cfg.cpp */
+    build_first(*this);
+    build_follow(*this);
 }
--- cfg.h
+++ cfg.h
@@ -12,25 +12,38 @@
 
 class cfg {
 
-    class symbol {
+public:
 
+    class symbol {
+    public:
         class production {
 
+            /* A production is represented by an ordered
+             *  list of symbols */
             std::list<symbol *> symbols;
+            
+            /* Each production has a first set, which is a set
+             * of terminals that can be matched from the non-terminal
+             * at a given input */
+            std::unordered_set<std::string> first;
+
+            /* Key of the nonterminal lhs of this production */
+            std::string lhs;
 
         public:
+            /* Appends symbol to production */
             void add_symbol(symbol *sym);
-
+            
+            /* Return list representation of produciton. */
             std::list<symbol *> get_symbols(void) const;
+            
+            /* Add a symbol to the first set of this production */
+            /* TODO: add_first can be implemented internally, hiding
+             *       implementaiton details, to disallow inconsistency 
+             *       caused by manipulation outside cfg class */
+            bool add_first(std::string _key);
         };
 
-        bool terminal;
-        std::string key;
-        std::list<production> productions;
-        std::unordered_set<std::string> first;
-        std::unordered_set<std::string> follow;
-
-    public:
         symbol(std::string _key, bool _terminal = false);
 
         bool is_terminal(void) const;
@@ -38,8 +51,6 @@ class cfg {
         std::string get_key(void) const;
 
         void add_production(production _production);
-
-        void add_first(std::string _key);
 
         void add_follow(std::string _key);
 
@@ -51,12 +62,16 @@ class cfg {
 
         std::unordered_set<std::string> get_follow() const;
 
+        std::list<production> get_productions();
+
+    private:
+        bool terminal;
+        std::string key;
+        std::list<production> productions;
+        std::unordered_set<std::string> follow;
+
     };
 
-    std::unordered_map<std::string, symbol> symbols;
-    std::string grammar;
-
-public:
     cfg(std::string _grammar);
 
     /* Adds and returns the symbol c by _key if it does not exist.
@@ -70,6 +85,18 @@ public:
      * */
     bool add_production(std::string lhs, std::list<std::string> &rhs);
 
+    symbol *get_symbol(std::string _key);
+
+    std::list<std::string> get_symbols();
+
+    /* Builds CFG by computing first and follow sets for
+     * each non-terminal, which are later used to build
+     * parsing table */
+    void build();
+
+private:
+    std::unordered_map<std::string, symbol> symbols;
+    std::string grammar;
 
 };
 
